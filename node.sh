ea74be24877078fe3dcbb5cfac445b8f75fbd194
obairnisarnajar <152411255+obairnisarnajar@users.noreply.github.com>
2025-04-18 15:58:49 +0530

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo "🌐 Starting Gensyn Node Setup by crypto_kasheer..."
 
-# Clone Gensyn source
 cd $HOME && rm -rf gensyn
 git clone https://github.com/zunxbt/gensyn-testnet.git gensyn
 cd gensyn
 
-# Give execution permission and run
 chmod +x gensyn.sh
 ./gensyn.sh
 
